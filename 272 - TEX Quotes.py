08bbba95dbfb5e3b4369b70c33dff18a24da5ccc
shiv03-tan <72273658+shiv03-tan@users.noreply.github.com>
2022-12-06 10:22:47 +0800

    Update 272 - TEX Quotes.py

--- 272 - TEX Quotes.py
+++ 272 - TEX Quotes.py
@@ -1,24 +1,20 @@
-
-list=[]
-count=0
-maxLength=0
+c = 0
 while True:
-    try:
-        n=input()
-        list.append(n)
-        count+=1
-        if maxLength<len(n):
-            maxLength=len(n)
-    
-    except EOFError:
-        for i in range(0,maxLength):
-            j=count-1
-            while j>=0:
-                if len(list[j])>i:
-                    print(list[j][i],end="")
-                else:
-                    print(" ",end="")
-                j=j-1
-            print()
-        break;
+	try :
+		n = input()
+	except EOFError:
+		break	
+	
+	a = []
+	for i in n:
+		if i == "\"":
+			if c == 0:
+				a.append("``")
+				c = 1
+			else:
+				a.append("''")
+				c = 0
+		else:
+			a.append(i)
+	print("".join(a))
         
