22149c3c17fe6e9ce28b1efcb852f2fd07a8b59b
shiv03-tan <72273658+shiv03-tan@users.noreply.github.com>
2023-03-09 18:20:23 +0800

    Update Bangla number.py

--- Bangla number.py
+++ Bangla number.py
@@ -1,74 +1,28 @@
-def f ( n ): 
-
-
-    if ( n >= 10000000 ):
-
-
-        f ( math . floor ( n / 10000000 ))
-
-
-        print ( 'kuti' , end = '' )
-
-
-        n %= 10000000
-
-
-    if ( n >= 100000 ):
-
-
-        f ( math . floor ( n / 100000 ))
-
-
-        print ( 'lakh' , end = '' )
-
-
-        n % = 100000
-
-
-    if ( n >= 1000 ):
-
-
-        f ( math . floor ( n / 1000 ))
-
-
-        print ( 'hajar' , end = '' )
-
-
-        n % = 1000
-
-
-    if ( n >= 100 ):
-
-
-        f ( math . floor ( n / 100 ))
-
-
-        print ( ' shata' , end = '' )
-
-
-        n % = 100
-
-
-    if ( n >= 1 ):
-
-
-        print ( f '  { n } ' , end = '' )
-
-
-import math 
-
-
-from sys import stdin   
-
-
-u = 1
-
-
-for s in stdin :   
-
-
-    n = int ( s )
-   
+def f(n):
+    if(n>=10000000):
+        f(math.floor(n/10000000))
+        print(' kuti',end='')
+        n%=10000000
+    if(n>=100000):
+        f(math.floor(n/100000))
+        print(' lakh',end='')
+        n%=100000
+    if(n>=1000):
+        f(math.floor(n/1000))
+        print(' hajar',end='')
+        n%=1000
+    if(n>=100):
+        f(math.floor(n/100))
+        print(' shata',end='')
+        n%=100
+    if(n>=1):
+        print(f' {n}',end='')
+import math
+from sys import stdin
+u=1
+for s in stdin:
+    n=int(s)
+    
     print("%4d." %u,end='')
     if(n>0):
         f(n)
